eb136c4de7c8cb46a2545e6b971caff35054bbc5
Betsimlv <123998141+Betsimlv@users.noreply.github.com>
2023-10-16 23:05:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import folium
 #credenciales = google.auth.default()
 #ruta = "gs://archivo_ml/data_sistema_final.parquet"
 #df = pd.read_parquet(ruta)
-df = pd.read_csv("data_sistema_final.parquet")
+df = pd.read_parquet("data_sistema_final.parquet")
 
 #Encabezado
 st.markdown('<h1 style="color: #20639B;text-align: center;">ANALYTICAL INSIGHTS CO.</h1>', unsafe_allow_html=True)
